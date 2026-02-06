d3cd9da66ff364aba983118a42bdad7372e65362
Denis <backwach1@yandex.ru>
2026-02-06 17:24:33 +0700

    Itteration 3

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -22,14 +22,22 @@ class RecipeFilter(django_filters.FilterSet):
     )
 
     def filter_is_favorited(self, queryset, name, value):
-        if value and hasattr(self, 'request') and self.request.user.is_authenticated:
-            favorites = Favorite.objects.filter(user=self.request.user).values_list('recipe_id', flat=True)
+        if (value and hasattr(self, 'request') and
+            self.request.user.is_authenticated):
+            favorites = (
+                Favorite.objects.filter(user=self.request.user)
+                .values_list('recipe_id', flat=True)
+            )
             return queryset.filter(id__in=favorites)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value and hasattr(self, 'request') and self.request.user.is_authenticated:
-            cart = ShoppingCart.objects.filter(user=self.request.user).values_list('recipe_id', flat=True)
+        if (value and hasattr(self, 'request') and
+            self.request.user.is_authenticated):
+            cart = (
+                ShoppingCart.objects.filter(user=self.request.user)
+                .values_list('recipe_id', flat=True)
+            )
             return queryset.filter(id__in=cart)
         return queryset
 
